e6ee2d2d465fd04c6bcd9a7ae08e83bb5bb29ca0
Kamil Witczak <222150@edu.p.lodz.pl>
2023-11-28 12:01:02 +0100

    Everything works, for 4

--- main.py
+++ main.py
@@ -34,53 +34,53 @@ items = [
 ]
 
 
+class Individual:
+    def __init__(self, genes):
+        self.genes = genes
+        self.adaptation = self.calculate_adaptation()
+        self.value = self.calculate_value()
+
+    def calculate_adaptation(self):
+        weight = 0
+        for gene, bit in enumerate(self.genes):
+            if bit == 1:
+                weight += items[gene][2]
+        return weight if weight <= backpack_capacity else 0
+
+    def calculate_value(self):
+        value = 0
+        for gene, bit in enumerate(self.genes):
+            if bit == 1:
+                value += items[gene][3]
+        return value
+
+    def mutate(self):
+        index = random.randint(0, len(self.genes) - 1)
+        if random.random() < will_mutate:
+            self.genes[index] = 1 - self.genes[index]  # Flipping the bit
+            self.adaptation = self.calculate_adaptation()  # Update adaptation after mutation
+            self.value = self.calculate_value()  # Update value after mutation
+
+    def __repr__(self):
+        return f"Genes: {self.genes}, Adaptation: {self.adaptation}, Value: {self.value}"
+
+
 def create_population(population_size, individual_size):
     population = []
-    for individual in range(0, population_size):
-        individual = []
-        for gene in range(0, individual_size):
-            gene = random.randint(0,1)
-            individual.append(gene)
+    for _ in range(population_size):
+        genes = [random.randint(0, 1) for _ in range(individual_size)]
+        individual = Individual(genes)
         population.append(individual)
     return population
 
 
-def measuring_adaptation(individual: []):
-    weight = 0
-    for gene in range(len(individual)):
-        bit = individual[gene]
-        if bit == 1:
-            weight = weight + items[gene][2]
-    if weight > backpack_capacity:
-        return 0
-    return weight
-
-
-def measuring_value(individual: []):
-    value = 0
-    for gene in range(len(individual)):
-        bit = individual[gene]
-        if bit == 1:
-            value = value + items[gene][3]
-    return value
-
-
-def adaptation_in_population(population):
-    overall = 0
-    for individual in population:
-        overall = overall + measuring_adaptation(individual)
-    for individual in population:
-        individual.append(measuring_adaptation(individual) / overall)
-    return overall
-
-
 def roulette(population, chosen_amount):
+    total_fitness = sum(individual.adaptation for individual in population)
     compartments = []
-    total_fitness = sum(individual[-1] for individual in population)
     sum_prob = 0
 
     for i, individual in enumerate(population):
-        probability = individual[-1] / total_fitness
+        probability = individual.adaptation / total_fitness
         compartments.append([i, sum_prob, sum_prob + probability])
         sum_prob += probability
 
@@ -96,25 +96,18 @@ def roulette(population, chosen_amount):
 
 
 def crossing_single_point(parent_a, parent_b, point):
+    genes_a = parent_a.genes
+    genes_b = parent_b.genes
     children = []
-    kid_1 = parent_a[:point+1] + parent_b[point+1:]
+    kid_1 = Individual(genes_a[:point+1] + genes_b[point+1:])
     children.append(kid_1)
-    kid_2 = parent_b[:point+1] + parent_a[point+1:]
+    kid_2 = Individual(genes_b[:point+1] + genes_a[point+1:])
     children.append(kid_2)
     return children
 
 
-def mutation(individual):
-    index = random.randint(0, len(individual)-1)
-    if random.random() < will_mutate:
-        if individual[index] == 1:
-            individual[index] = 0
-        else:
-            individual[index] = 1
-
-
 def best_in_population(population):
-    max_element = max(population, key=lambda x: x[-1])
+    max_element = max(population, key=lambda x: x.adaptation)
     return max_element
 
 
@@ -125,17 +118,12 @@ def cut_from_population(population_size, population):
 
 def ag_roulette_single_point(population_size, generations_amount, parents_amount):
     global_pop = create_population(population_size, 26)
-    best_kid = []
-    for generation in range(0, generations_amount):
-        adaptation_in_population(global_pop)
+    best_kid = None
+    for generation in range(generations_amount):
         best_in_pop = best_in_population(global_pop)
-        if best_in_pop[:-1] > best_kid[:-1]:
+        if not best_kid or best_in_pop.adaptation > best_kid.adaptation:
             best_kid = best_in_pop
         parents = roulette(global_pop, parents_amount)
-        for kid in parents:
-            kid.pop()
-        for ind in global_pop:
-            ind.pop()
         children = []
         for parent_a in parents:
             for parent_b in parents:
@@ -143,47 +131,14 @@ def ag_roulette_single_point(population_size, generations_amount, parents_amount
                     resulting_children = crossing_single_point(parent_a, parent_b, 12)
                     children.extend(resulting_children)
         for kid in children:
-            mutation(kid)
+            kid.mutate()
         global_pop.extend(children)
         global_pop = cut_from_population(population_size, global_pop)
     return best_kid
 
 
-# def ag_(population_size, generations_amount, parents_amount):
-#     global_pop = create_population(population_size, 26)
-#     best_kid = []
-#     for generation in range(0, generations_amount):
-#         adaptation_in_population(global_pop)
-#         best_in_pop = best_in_population(global_pop)
-#         if best_in_pop[:-1] > best_kid[:-1]:
-#             best_kid = best_in_pop
-#         parents = roulette(global_pop, parents_amount)
-#         for kid in parents:
-#             kid.pop()
-#         children = []
-#         for parent_a in parents:
-#             for parent_b in parents:
-#                 if parent_a != parent_b:
-#                     resulting_children = crossing_single_point(parent_a, parent_b, 12)
-#                     children.extend(resulting_children)
-#         for kid in children:
-#             mutation(kid)
-#         global_pop = parents + children
-#     return best_kid
-
-
 if __name__ == '__main__':
-    result = ag_roulette_single_point(1000, 20, 60)
-    value = measuring_value(result)
+    result = ag_roulette_single_point(5000, 30, 80)
     print("____________Wynik dla selekcji ruletki oraz krzyżowania jednopunktowego____________")
     print("Najlepsze dziecko: ", result)
-    print("Wartość przedmiotów: ", value)
-
-
-
-
-
-
-
-
-
+    print("Wartość przedmiotów: ", result.value)
